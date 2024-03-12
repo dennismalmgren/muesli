70dc96e50d7479c536e6b65ad23f49afcc86b81c
dennismalmgren <dennis@dennismalmgren.se>
2024-03-13 00:10:56 +0100

    Fixed it

--- run.sh
+++ run.sh
@@ -28,4 +28,7 @@ cd /proj/berzelius-aiics-real/users/x_denma/
 # Execute my Apptainer image binding in the current working directory
 # containing the Python script I want to execute
 cmd="apptainer exec --env WANDB_API_KEY='f832ecbebaa081e6438201bd475fe26f9f0b1d82' --nv -B projs/muesli:/app berzdev_latest.sif bash -c 'cd /app/path_integration_module && python train_energy_predictor.py'"
+cmd="apptainer exec --env WANDB_API_KEY='f832ecbebaa081e6438201bd475fe26f9f0b1d82' --nv -B projs/muesli:/app berzdev_latest.sif bash -c 'cd /app/path_integration_module && python generate_random_trajectories.py'"
+cmd="apptainer exec --env WANDB_API_KEY='f832ecbebaa081e6438201bd475fe26f9f0b1d82' --nv -B projs/muesli:/app berzdev_latest.sif bash -c 'cd /app/ppo_grid && python ppo_grid.py'"
+cmd="apptainer exec --env WANDB_API_KEY='f832ecbebaa081e6438201bd475fe26f9f0b1d82' --nv -B projs/muesli:/app berzdev_latest.sif bash -c 'cd /app/ppo && python ppo.py'"
 eval $cmd
