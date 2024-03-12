69edf25655bff9548caed9c7508dd26cba5cacb9
dennismalmgren <dennis@dennismalmgren.se>
2024-03-12 23:59:31 +0100

    Fixed config

--- run.sh
+++ run.sh
@@ -20,17 +20,12 @@ while getopts ":s:" opt; do
   esac
 done
 
-# Set default values if arguments are not provided
-seed=42
-model_type="dt2"
-augment_fraction=0.0
-filter_dataset=True
 
 # Apptainer images can only be used outside /home. In this example the
 # image is located here
-cd /proj/berzelius-aiics-real/users/x_denma/projs/ocmdt
+cd /proj/berzelius-aiics-real/users/x_denma/
 
 # Execute my Apptainer image binding in the current working directory
 # containing the Python script I want to execute
-cmd="enroot start -e WANDB_API_KEY='f832ecbebaa081e6438201bd475fe26f9f0b1d82' --rw --mount .:/app devcontainer sh -c 'python dt_main.py replay_buffer.filter_dataset=$filter_dataset replay_buffer.augment_fraction=$augment_fraction env.seed=$seed model.model_type=$model_type logger.mode=online'"
+cmd="apptainer exec --nv -B projs/muesli:/app berzdev_latest.sif bash -c 'cd path_integration_module && python train_energy_predictor.py'"
 eval $cmd
